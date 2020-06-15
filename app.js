de2504b53d2cddb03addf839266fd9bbe8cfed97
mirzahamza1 <56633470+mirzahamza1@users.noreply.github.com>
2020-06-15 10:38:21 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,53 @@
+
+
+alert("Eror! please enter a valid password")
+
+alert("welcome to JS land... \nHappy coding! ")
+
+alert("welcome to JS land...")
+
+alert("Happy coding \nPrevent this page from additional dialouges ")
+
+console.log ("Hello i can use my JS through my web browser console")
+
+
+
+var message;
+message = "hello world";
+alert (message)
+
+
+
+var name,age,certify
+name = "ali mughal";
+age ="50 years";
+certify ="mobile web certified";
+alert(name)
+alert(age)
+alert(certify)
+
+
+var pizza;
+pizza = "pizza\nizza\nzza\nza\na"
+alert(pizza)
+
+
+
+var username;
+username = "google@gmail.com"
+alert("my email address is " + username );
+
+
+var design;
+design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert(design);
+console.log(design);
+
+var htmltext;
+htmltext = "i can write html text by using js"
+document.write(htmltext);
+
+
+
+
+
